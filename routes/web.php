6a9892a83dd16d875d00dcfcea7f0fdf7e282ef1
truong <truong.nguyen200207@vnuk.edu.vn>
2022-11-05 13:41:42 +0700

    first

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Http\Controllers\PageController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/trangchu',[PageController::class, 'getIndex']);
+Route::get('/type/{id}',[PageController::class, 'getLoaiSp']);
+Route::get('/detail/{id}',[PageController::class, 'getDetail']);
+
+Route::get('/contact', [PageController::class, 'getContact']);
+Route::get('/about',[PageController::class, 'getAbout']);
